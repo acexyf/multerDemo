dcd073d7438fce0c72a16272b81a97afd7ee0386
xyf17469 <xyf17469@ly.com>
2017-09-25 11:40:34 +0800

    demo1完成

--- demo1/demo1.js
+++ demo1/demo1.js
@@ -1,23 +1,13 @@
-
 var express = require('express');
 var fs =require('fs');
 var multer = require('multer');
 var path = require('path');
-
 var ipaddress = getIPAdress();
 var port = 8093;
-
-
 var app = express();
 
 
 var upload = multer({
-    limits: {
-        //在multipart表单中, 文件最大数量
-        files: 10,
-        //在multipart表单中, 文件最大长度 (字节单位)
-        fileSize: 1000*1000*5
-    },
     storage: multer.diskStorage({
         destination: function (req, file, cb) {
             cb(null, './uploads/')
@@ -26,10 +16,7 @@ var upload = multer({
             var changedName = (new Date().getTime())+'-'+file.originalname;
             cb(null, changedName);
         }
-    }),
-    fileFilter: function(req, file, cb){
-        cb(null, true);
-    }
+    })
 });
 
 app.get('/', function (req, res) {
@@ -37,7 +24,7 @@ app.get('/', function (req, res) {
 });
 
 //单个文件上传
-app.post('/upload/single',upload.single('singleFile'), function(req,res){
+app.post('/upload/single',upload.single('singleFile'), (req,res)=>{
     console.log(req.file);
     res.json({
         code: '0000',
@@ -47,32 +34,28 @@ app.post('/upload/single',upload.single('singleFile'), function(req,res){
 });
 
 //多个文件上传
-app.post('/upload/multer',upload.array('multerFile'), function(req,res){
+app.post('/upload/multer',upload.array('multerFile'), (req,res)=>{
     console.log(req.files);
-
     let fileList = [];
-    req.files.map(function(elem){
+    req.files.map((elem)=>{
         fileList.push({
             originalname: elem.originalname
         })
     });
-
     res.json({
         code: '0000',
         type:'multer',
         fileList:fileList
-    })
+    });
 });
 
 
 let server = app.listen(port, function () {
-    
     if (ipaddress) {
         console.log('please open ' + ipaddress + ':' + port + ' in browser');
     } else {
         console.log('no networking, please open ' + ipaddress + ':' + port + ' in browser')
     }
-
 });
 
 /**
